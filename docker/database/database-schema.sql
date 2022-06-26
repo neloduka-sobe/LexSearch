f65613e5c710d893a5352f8d85dec37c82c6a78f
Neloduka Sobe <neloduka_sobe@protonmail.ch>
2022-06-26 10:27:07 +0200

    Outlined  database layout and added first table

--- docker/database/database-schema.sql
+++ docker/database/database-schema.sql
@@ -1,3 +1,21 @@
-DROP TABLE IF EXISTS ``;
-CREATE TABLE `` (
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+use lexsearch;
+DROP TABLE IF EXISTS `guests`;
+CREATE TABLE `guests` (
+	`guest_id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+	PRIMARY KEY (`guest_id`)
+);
+
+
+DROP TABLE IF EXISTS `episodes`;
+CREATE TABLE `episodes` (
+	`episode_id` int(11) NOT NULL AUTO_INCREMENT,
+	`number` int(11) NOT NULL,
+	`title` varchar(128) NOT NULL COLLATE utf8mb4_unicode_ci,
+	`yt_id` char(11) NOT NULL COLLATE utf8mb4_unicode_ci,
+	`guests` set NOT NULL DEFAULT set(),
+	/*`episode_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,*/
+	`description` text COLLATE utf8mb4_unicode_ci,
+	PRIMARY KEY (`episode_id`)
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
