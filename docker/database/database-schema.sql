30dcd21382de14c0061771eccb15b368af003197
Neloduka Sobe <neloduka_sobe@protonmail.ch>
2022-06-27 18:19:08 +0200

    Changed database-schema.sql file

--- docker/database/database-schema.sql
+++ docker/database/database-schema.sql
@@ -13,9 +13,26 @@ CREATE TABLE `episodes` (
 	`number` int(11) NOT NULL,
 	`title` varchar(128) NOT NULL COLLATE utf8mb4_unicode_ci,
 	`yt_id` char(11) NOT NULL COLLATE utf8mb4_unicode_ci,
-	`guests` json,
 	`episode_date` timestamp default CURRENT_TIMESTAMP,
 	`description` text COLLATE utf8mb4_unicode_ci,
 	PRIMARY KEY (`episode_id`)
+); 
+
+DROP TABLE IF EXISTS `appearances`;
+CREATE TABLE `appearances` (
+	`appearance_id` int(11) NOT NULL AUTO_INCREMENT,
+	`episode_id` int(11) NOT NULL,
+	`guest_id` int(11) NOT NULL,	
+	PRIMARY KEY (`appearance_id`)
+);
+
+DROP TABLE IF EXISTS `timestamps`;
+CREATE TABLE `timestamps` (
+	`timestamp_id` int(11) NOT NULL AUTO_INCREMENT,
+	`episode_id` int(11) NOT NULL,
+	`full_text` mediumtext NOT NULL COLLATE utf8mb4_unicode_ci,
+	`timestamp` json,
+	`last_edit_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`timestamp_id`)
 ); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
